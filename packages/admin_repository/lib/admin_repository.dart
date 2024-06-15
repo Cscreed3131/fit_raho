7341341ab009bd1a96b20d236cccdb2b97432138
Cscreed3131 <anubhav82355@gmail.com>
2024-06-15 11:09:21 +0530

    intial commit of the day

--- packages/admin_repository/lib/admin_repository.dart
+++ packages/admin_repository/lib/admin_repository.dart
@@ -3,4 +3,4 @@ library admin_repository;
 export 'src/entities/entities.dart';
 export 'src/models/models.dart';
 export 'src/admin_repo.dart';
-// export 'src/firebase_admin_repo.dart';
+export 'src/firebase_admin_repo.dart';
--- packages/admin_repository/lib/src/admin_repo.dart
+++ packages/admin_repository/lib/src/admin_repo.dart
@@ -1,7 +1,11 @@
 import 'package:trainer_repository/trainer_repository.dart';
 import 'package:user_repository/user_repository.dart';
 
+import '../admin_repository.dart';
+
 abstract class AdminRepo {
+  Stream<Admin?> get owner;
+
   Future<List<MyUser?>> getUsers();
 
   Future<List<Trainer>> getTrainers();
